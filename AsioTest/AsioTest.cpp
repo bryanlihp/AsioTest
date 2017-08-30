1f3d0334c85980122896247645ca55e491f52f6e
Bryan Li <Bryan Li@BRYANDEV4>
2017-08-30 13:01:40 -0700

    hotfix 1.1 implemented

--- AsioTest/AsioTest.cpp
+++ AsioTest/AsioTest.cpp
@@ -17,7 +17,9 @@ int GetWaitTime(int nMin, int nMax)
 
 void Test1()
 {
+	//test1_1 HOTFIX APPLIED
 	boost::asio::io_service svc;
+	// up to this line
 	svc.run();
 	std::cout << "Do you reckon this line displays?" << std::endl; // run returns immediately as there is no work to do
 }
